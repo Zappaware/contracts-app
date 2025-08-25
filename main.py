9ab2fee11a406717d81d9e83fb236dc389c4a7d0
Sergio Cervantes <sergio_germanc@hotmail.com>
2025-08-25 00:47:41 -0600

    add new route to main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from nicegui import ui
 from components.header import header
 from pages.home_page import home_page
 from pages.new_contract import new_contract
+from pages.new_vendor import new_vendor
 
 
 @ui.page("/")
@@ -20,7 +21,7 @@ def new_contract_page():
 @ui.page("/new-vendor")
 def new_vendor_page():
     header()
-    ui.label("New Vendor Page - Under Construction")
+    new_vendor()
 
 
 ui.run(title="Aruba Bank", port=5000)
