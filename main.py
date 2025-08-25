9f128ca59f72892c3716c5857b273f7be41e31a2
Sergio Cervantes <sergio_germanc@hotmail.com>
2025-08-25 07:50:01 -0600

    Add more pages to the project

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from pages.home_page import home_page
 from pages.new_contract import new_contract
 from pages.new_vendor import new_vendor
 from pages.login import login_page
+from pages.terminated_contracts import terminated_contracts
+from pages.active_contracts import active_contracts
+from pages.pending_contracts import pending_contracts
+from pages.expired_contracts import expired_contracts
+
 
 @ui.page("/")
 def main_page():
@@ -32,5 +37,25 @@ def new_vendor_page():
 @ui.page("/login")
 def login_route():
     login_page()
+
+@ui.page("/terminated-contracts")
+def terminated_contracts_page():
+    header()
+    terminated_contracts()
+
+@ui.page("/active-contracts")
+def active_contracts_page():
+    header()
+    active_contracts()
+
+@ui.page("/pending-contracts")
+def pending_contracts_page():
+    header()
+    pending_contracts()
+
+@ui.page("/expired-contracts")
+def expired_contracts_page():
+    header()
+    expired_contracts()
 
 ui.run(title="Aruba Bank", port=5000, storage_secret="aruba_bank_secret")
